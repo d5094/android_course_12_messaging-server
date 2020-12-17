321bba9d5c87d3cad64668e9d95e9cf7f067c9ba
Aswin <aswin.wibisurya@weekendinc.com>
2020-12-17 21:11:07 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "YOUR_DATABASE_URL"
+});
+
+
+// This registration token comes from the client FCM SDKs.
+var registrationToken = 'YOUR_REGISTRATION_TOKEN';
+
+var message = {
+  data: {
+    score: '850',
+    time: '2:45'
+  },
+  token: registrationToken
+};
+
+// Send a message to the device corresponding to the provided
+// registration token.
+admin.messaging().send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
